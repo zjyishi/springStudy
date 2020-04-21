b0d563580a1083b8647093a9ff76dce208917e32
zjyishi <zjyishi@163.com>
2020-04-21 20:58:16 +0800

    引入数据库

---
+++ taco-cloud/src/main/java/tacos/IngredientRepository.java
@@ -0,0 +1,7 @@
+package tacos;
+import tacos.Ingredient;
+public interface IngredientRepository {
+	Iterable<Ingredient> findAll();
+	Ingredient findOne(String id);
+	Ingredient save(Ingredient ingredient);
+}
